8635f5a91716a2bf122cc39801d59ccda136980e
GaryCarberry <carbury.gary@gmail.com>
2025-09-30 21:53:34 +0100

    Update CSV_Capture_File.py

--- CSV_Capture_File.py
+++ CSV_Capture_File.py
@@ -1,6 +1,6 @@
-import csv
+import csv#allows reading of csv
 import time
-import requests
+import requests#allows the coe to make HTTP requests
 import random
 
 
@@ -12,9 +12,15 @@ with open ('microbit(4).csv',mode='r')as file:
         dist=lines[3]
         sound=lines[2]
         moisture=random.randint(1,100)
-        msg=("https://api.thingspeak.com/update?api_key=GQVT0LGOT5K9LKRA&field1={},&field2={},&field3={},&field4={},&field5={}".format(timest,temp,dist,sound,moisture))
+        msg=("https://api.thingspeak.com/update?api_key=GQVT0LGOT5K9LKRA&
+        field1={},#modfies each field in thingspeak to showcase data received from the microbit
+        &field2={},
+        &field3={},
+        &field4={},
+        &field5={}".format(timest,temp,dist,sound,moisture))
         print(msg)
         api_url=msg
         response=requests.get(api_url)
         print(lines)
         time.sleep(20)
+
